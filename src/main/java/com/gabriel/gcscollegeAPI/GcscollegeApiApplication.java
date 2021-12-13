ca7a3303aebbd69467168b56cb225f3f5ec9354d
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-12-13 21:29:57 +0000

    brand new project

---
+++ src/main/java/com/gabriel/gcscollegeAPI/GcscollegeApiApplication.java
@@ -0,0 +1,18 @@
+package com.gabriel.gcscollegeAPI;
+
+import java.util.Arrays;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class GcscollegeApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GcscollegeApiApplication.class, args);
+	}
+	
+}
---
+++ src/test/java/com/gabriel/gcscollegeAPI/GcscollegeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gabriel.gcscollegeAPI;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcscollegeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
