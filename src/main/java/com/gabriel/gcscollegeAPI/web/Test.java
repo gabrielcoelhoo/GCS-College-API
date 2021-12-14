daed8ee2b4f711d86d6faa55d4e92e714e90fd3c
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-12-14 00:18:18 +0000

    booking method created to text connection

--- src/main/java/com/gabriel/gcscollegeAPI/web/Test.java
+++ src/main/java/com/gabriel/gcscollegeAPI/web/Test.java
@@ -1,14 +1,34 @@
 package com.gabriel.gcscollegeAPI.web;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("api")
 public class Test {
 	
 	@RequestMapping("/")
 	public String getGreeting() {
 		return "helooooo";
+	}
+	
+	@GetMapping("userDetails")
+	public void login(String email, String password) {
+
+	System.out.println(email);
+	System.out.println(password);
+	}
+	
+	@GetMapping("bookingDetails")
+	public void login(String englishCourse, String accomodation, String transfer, String comments) {
+
+	System.out.println(englishCourse);
+	System.out.println(accomodation);
+	System.out.println(transfer);
+	System.out.println(comments);
 	}
 
 }
