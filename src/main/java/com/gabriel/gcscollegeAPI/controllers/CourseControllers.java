b2717a2b9760e51e39f63d97e7e5ec6ea0398e43
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-12-30 21:20:38 +0000

    course controllers tested through postman

--- src/main/java/com/gabriel/gcscollegeAPI/controllers/CourseControllers.java
+++ src/main/java/com/gabriel/gcscollegeAPI/controllers/CourseControllers.java
@@ -26,6 +26,8 @@ import com.gabriel.gcscollegeAPI.services.EmployeeServiceImpl;
 @CrossOrigin(origins = "*")
 @RequestMapping("api/courses")
 public class CourseControllers {
+	
+	//all methods verified through postman
 
 	@Autowired
 	private CourseServiceImpl courseService;
@@ -35,140 +37,52 @@ public class CourseControllers {
 		
 	private Employee employee;
 
-	private int sizeOfList;
-
-	
-
-	
-	// I KNOW THAT THERE IS A LOT OF COMMENTS
-	// BUT AS SOON AS I HAVE AN IDEA I NEED TO DECLARE IT
-	//I WILL REMOVE MOST OF THEM WHEN I FINISH
-	
-	
-
 	// working on this
 	// -get a course and check if there is any available spot
 	// 10 slots per sales person
 	// 4 persons in total
 	
-	//flow
-	//check spots availability? 
-	
-	//if yes send to a specific employee
-	//save in employee course db 
-	//save in course db
-	
-	//else
-	//return a message "there is not availability at the moment"
-	// in the future save it and set in a queue
-	
-
 	// in the future let this amount of slots and sales dynamically
 	// regarding adm orders
 
 	// where will I create the method to this verification before submitcourse ?
 
-	@PostMapping
+	@PostMapping("/submitCourse")
 	@ResponseStatus(value = HttpStatus.CREATED)
-	public Course booking(@RequestBody Course course) {
-		
-		///  CREATE EMPLOY CLASSES
-		
-//		sizeOfList = courseService.getCourses().size();
-//		
-//		int sizeOfListNext = courseService.getCourses().size() + 1;
-//		
-//		System.out.println("course id = " + courseService.getCourseById(sizeOfList).getId());
-//		
-		//System.out.println("employee id = " + employee);
-		
-		return courseService.save(course);
-		
-
+	public String booking(@RequestBody Course course) {	
+		 courseService.save(course);
+		 return "this course has been created successfully";
 	}
-		
-		//System.out.println("employee id " + employee.getId());
-		
-				//return courseService.saveCourse(course);
 
-//		if (quantity <= 10) {
-//
-//			if (employee.getId() == 1) {
-//
-//				employeeCourseServiceImpl.saveEmployeeCourse(employeeCourse);
-//
-//			}
-//		} else if (quantity >= 11 & quantity <= 20) {
-//			
-//			if (employee.getId() == 2) {
-//
-//				employeeCourseServiceImpl.saveEmployeeCourse(employeeCourse);
-//
-//			}
-//
-//		} else if (quantity >= 21 & quantity <= 30) {
-//			
-//			if (employee.getId() == 3) {
-//
-//				employeeCourseServiceImpl.saveEmployeeCourse(employeeCourse);
-//
-//			}
-//
-//		} else if (quantity >= 31 & quantity <= 40) {
-//			
-//			if (employee.getId() == 4) {
-//
-//				employeeCourseServiceImpl.saveEmployeeCourse(employeeCourse);
-//
-//			}
-//
-//		} else {
-//			// selles are fully booked
-//		}
-//
-//		System.out.println(quantity);
-
-
-	
-	
-//	public boolean hasAvailableSpots(Course course, Employee employee) {
-//		
-//		
-//		courseService.getCourseById(course.getId());
-//		employee
-//
-//		
-//		return true;
-//	}
-//	
-	
-
-	@GetMapping
+	@GetMapping("/all")
 	public List<Course> findAllCourses() {
 		return courseService.findAll();
 	}
 	
+	//courses created at the moment
 	@GetMapping("/validCouses")
 	public List<Course> findAllValidCourses() {
 		return courseService.findAllValidCourses();
 	}
 
-	@GetMapping("/{idCourse}")
-	public Course findCourseById(@PathVariable Long id) {
-		return courseService.findOrThrowsException(id);
+	@GetMapping("/getById/{idCourse}")
+	public Course findCourseById(@PathVariable Long idCourse) {
+		return courseService.findOrThrowsException(idCourse);
 	}
 
 	@PutMapping("/{idCourse}")
-	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public Course updateProduct(@RequestBody @Valid Course course, @PathVariable Long idCourse) {
+	@ResponseStatus(value = HttpStatus.ACCEPTED)
+	public String updateProduct(@RequestBody @Valid Course course, @PathVariable Long idCourse) {
 		courseService.findOrThrowsException(idCourse);
-		return courseService.update(course);
+		 courseService.update(course);
+		 return "this course has been updated successfully";
 	}
 
 	@DeleteMapping("/{idCourse}")
-	@ResponseStatus(value = HttpStatus.NO_CONTENT)
-	public void deleteProduct(@PathVariable Long idCourse) {
+	@ResponseStatus(value = HttpStatus.OK)
+	public String deleteProduct(@PathVariable Long idCourse) {
 		courseService.deleteCourse(idCourse);
+		return "this course has been deleted successfully";
 
 	}
 
