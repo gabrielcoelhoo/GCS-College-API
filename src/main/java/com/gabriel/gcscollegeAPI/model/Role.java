b7d5183e09e32988c3ea3acc29c181a3344accc8
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2022-01-06 19:49:24 +0000

    role

---
+++ src/main/java/com/gabriel/gcscollegeAPI/model/Role.java
@@ -0,0 +1,25 @@
+package com.gabriel.gcscollegeAPI.model;
+
+@Entity
+public class Role {
+
+    @Id
+    private String roleName;
+    private String roleDescription;
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public String getRoleDescription() {
+        return roleDescription;
+    }
+
+    public void setRoleDescription(String roleDescription) {
+        this.roleDescription = roleDescription;
+    }
+}
