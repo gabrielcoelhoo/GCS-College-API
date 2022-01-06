fc8e82ad55f8de460a9f71737cfad084d70a19a0
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2022-01-06 19:42:35 +0000

    RoleRepository

---
+++ src/main/java/com/gabriel/gcscollegeAPI/repositories/RoleReository.java
@@ -0,0 +1,8 @@
+package com.gabriel.gcscollegeAPI.repositories;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoleReository extends CrudRepository<Role, String> {
+
+}
